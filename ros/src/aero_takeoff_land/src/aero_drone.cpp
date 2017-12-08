51890649b4e55f09f338665ed7823154e00845d2
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2017-12-08 17:33:34 +0530

    aero_takeoff_land: Refactoring.
    1. AeroDrone class abstracts Intel Aero Drone actions arm, takeoff and land.
    2. Updated maintainer in package.xml
    3. Cmake changes.

---
+++ ros/src/aero_takeoff_land/src/aero_drone.cpp
@@ -0,0 +1,142 @@
+
+#include "aero_drone.h"
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <ros/ros.h>
+
+AeroDrone::AeroDrone()
+{
+  resetHome();
+  getHomeGps();
+}
+
+AeroDrone::~AeroDrone()
+{
+  delete thread_watch_alt_;
+}
+
+bool AeroDrone::arm()
+{
+  if (!is_home_gps_set_)
+  {
+    ROS_ERROR("Fatal: Can't arm: No GPS Fix!");
+    return false;
+  }
+
+  auto arming_client = nh_.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
+  mavros_msgs::CommandBool srv_arm;
+  srv_arm.request.value = true;
+  if (arming_client.call(srv_arm) && srv_arm.response.success)
+    ROS_DEBUG("Arm sent");
+  else
+  {
+    ROS_ERROR("Arming failed, exiting.");
+    ros::shutdown();
+    return false;
+  }
+  return true;
+}
+
+bool AeroDrone::takeoff()
+{
+  if (!is_home_gps_set_)
+  {
+    ROS_ERROR("Fatal: Can't takeoff: No GPS Fix!");
+    return false;
+  }
+
+  auto takeoff_client = nh_.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
+  mavros_msgs::CommandTOL srv_takeoff{};
+
+  srv_takeoff.request.altitude = home_gps_.altitude;
+  srv_takeoff.request.latitude = home_gps_.latitude;
+  srv_takeoff.request.longitude = home_gps_.longitude;
+
+  // Thread that watch for change in altitude of Aero.
+  thread_watch_alt_ = new boost::thread(boost::bind(&AeroDrone::watchAltitudeThread, this));
+
+  if (takeoff_client.call(srv_takeoff) && srv_takeoff.response.success)
+    ROS_INFO("Takeoff sent");
+  else
+  {
+    ROS_ERROR("Takeoff failed, exiting.");
+    ros::shutdown();
+    return false;
+  }
+  return true;
+}
+
+bool AeroDrone::land()
+{
+  if (!is_home_gps_set_)
+  {
+    ROS_ERROR("Fatal: Can't takeoff: No GPS Fix!");
+    return false;
+  }
+
+  auto land_client = nh_.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
+  mavros_msgs::CommandTOL srv_land{};
+
+  if (land_client.call(srv_land) && srv_land.response.success)
+    ROS_INFO("Land sent");
+  else
+  {
+    ROS_ERROR("Landing failed, exiting.");
+    ros::shutdown();
+    return false;
+  }
+  return true;
+}
+
+void AeroDrone::resetHome()
+{
+  home_gps_.latitude = home_gps_.longitude = home_gps_.altitude = NAN;
+}
+
+void AeroDrone::getHomeGps()
+{
+  // FCU Home position: See http://docs.ros.org/api/sensor_msgs/html/msg/NavSatFix.html
+  auto home_gps_sub = nh_.subscribe<sensor_msgs::NavSatFix>("mavros/global_position/global", 1,
+                                                            boost::bind(&AeroDrone::setHomeGpsCB, this, _1));
+
+  ROS_INFO("Waiting for Aero FC Home to be set...");
+  while (ros::ok() && !is_home_gps_set_)
+  {
+    ros::spinOnce();
+    rate_.sleep();
+  }
+}
+
+// Callback that gets called periodically from MAVROS notifying Global Poistion of Aero FCU
+void AeroDrone::setHomeGpsCB(const sensor_msgs::NavSatFixConstPtr& msg)
+{
+  home_gps_ = *msg;
+  is_home_gps_set_ = true;
+  ROS_INFO("Received Home: %lf, %lf, %lf", home_gps_.latitude, home_gps_.longitude, home_gps_.altitude);
+}
+
+// printing altitude
+void AeroDrone::getRelativeAltitudeCB(const std_msgs::Float64ConstPtr& msg, std_msgs::Float64* relative_altitude)
+{
+  *relative_altitude = *msg;
+  ROS_INFO("Altitude: [%g]", relative_altitude->data);
+}
+
+// subscribing to mavros/global_position/rel_alt to get altitude updates
+void AeroDrone::watchAltitudeThread()
+{
+  std_msgs::Float64 relative_altitude;
+  auto node = boost::make_shared<ros::NodeHandle>();
+  auto relative_pos_sub =
+      node->subscribe<std_msgs::Float64>("mavros/global_position/rel_alt", 1,
+                                         boost::bind(&AeroDrone::getRelativeAltitudeCB, this, _1, &relative_altitude));
+  ros::Rate rate(1.0);
+
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    rate.sleep();
+  }
+}
--- ros/src/aero_takeoff_land/src/aero_takeoff_land_node.cpp
+++ ros/src/aero_takeoff_land/src/aero_takeoff_land_node.cpp
@@ -1,100 +1,30 @@
 /**
-*@file aero_takeoff_land_node.cpp
-*@brief Demonstration of takeoff and land using mavros
-*@date 2017-10-24
+ * @file aero_takeoff_land_node.cpp
+ * @brief Demonstration of Intel Aero takeoff and land using mavros.
+ * @date 2017-10-24
 */
 
-#include <boost/bind.hpp>
-#include <boost/thread/thread.hpp>
 #include <cstdlib>
-#include <mavros_msgs/CommandBool.h>
-#include <mavros_msgs/CommandTOL.h>
-#include <mavros_msgs/SetMode.h>
-#include <mavros_msgs/State.h>
-#include <ros/ros.h>
-#include <sensor_msgs/NavSatFix.h>
-#include <std_msgs/Float64.h>
-#include <string.h>
-
-// printing altitude
-void getRelativeAltitudeCb(const std_msgs::Float64ConstPtr& msg, std_msgs::Float64* relative_altitude)
-{
-  *relative_altitude = *msg;
-  ROS_INFO("Altitude: [%g]", relative_altitude->data);
-}
-
-// subscribing to mavros/global_position/rel_alt to get altitude updates
-void watchFlightAltitude()
-{
-  std_msgs::Float64 relative_altitude;
-  ros::NodeHandlePtr node = boost::make_shared<ros::NodeHandle>();
-  ros::Subscriber relative_pos_sub = node->subscribe<std_msgs::Float64>(
-      "mavros/global_position/rel_alt", 1, boost::bind(getRelativeAltitudeCb, _1, &relative_altitude));
-  ros::Rate rate(1);
-  while (ros::ok())
-  {
-    ros::spinOnce();
-    rate.sleep();
-  }
-}
+#include "aero_drone.h"
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "aero_takeoff_land");
-  ros::NodeHandle nh;
+  AeroDrone aero;
 
-  // Thread that watch for change in altitude of Aero.
-  boost::thread thread_altitude(watchFlightAltitude);
-  const float ALTITUDE = 488.00;
-  const float LATITUDE = 47.3977415;
-  const float LONGITUDE = 8.5455937;
-  const float MIN_PITCH = 0.f;
-  const float YAW = 0.f;
+  if (!aero.arm())
+    exit(EXIT_FAILURE);
 
-  // arming
-  ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
-  mavros_msgs::CommandBool srv_arm;
-  srv_arm.request.value = true;
-  if (arming_client.call(srv_arm) && srv_arm.response.success)
-    ROS_INFO("Arm sent %d", srv_arm.response.success);
-  else
-  {
-    ROS_ERROR("Arming failed, exiting.");
-    ros::shutdown();
-    return -1;
-  }
+  if (!aero.takeoff())
+    exit(EXIT_FAILURE);
 
-  // takeoff
-  ros::ServiceClient takeoff_client = nh.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
-  mavros_msgs::CommandTOL srv_takeoff;
-  srv_takeoff.request.altitude = ALTITUDE;
-  srv_takeoff.request.latitude = LATITUDE;
-  srv_takeoff.request.longitude = LONGITUDE;
-  srv_takeoff.request.min_pitch = MIN_PITCH;
-  srv_takeoff.request.yaw = YAW;
-  if (takeoff_client.call(srv_takeoff) && srv_takeoff.response.success)
-    ROS_INFO("Takeoff sent %d", srv_takeoff.response.success);
-  else
-  {
-    ROS_ERROR("Takeoff failed, exiting.");
-    ros::shutdown();
-    return -1;
-  }
   sleep(5);  // Let Aero reach takeoff altitude
 
-  // land
-  ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
-  mavros_msgs::CommandTOL srv_land{};
-  if (land_client.call(srv_land) && srv_land.response.success)
-    ROS_INFO("Land sent %d", srv_land.response.success);
-  else
-  {
-    ROS_ERROR("Landing failed, exiting.");
-    ros::shutdown();
-    return -1;
-  }
+  if (!aero.land())
+    exit(EXIT_FAILURE);
 
   sleep(5);  // Let Aero land..
+
   ROS_INFO("Done");
   ros::shutdown();
 
