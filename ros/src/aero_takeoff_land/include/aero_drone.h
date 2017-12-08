2308eb8c79fb894d6a827b853bfd4400c6bdc9df
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2017-12-08 17:40:14 +0530

    aero_takeoff_land: re-organising AeroDrone class.

--- ros/src/aero_takeoff_land/include/aero_drone.h
+++ ros/src/aero_takeoff_land/include/aero_drone.h
@@ -10,25 +10,26 @@
 class AeroDrone
 {
 public:
+  AeroDrone();
+  ~AeroDrone();
+
+  // Actions
   bool arm();
   bool takeoff();
   bool land();
-  AeroDrone();
-  ~AeroDrone();
 
 private:
   void resetHome();
   void getHomeGps();
   void setHomeGpsCB(const sensor_msgs::NavSatFixConstPtr& msg);
+  void watchAltitudeThread();
+  void getRelativeAltitudeCB(const std_msgs::Float64ConstPtr& msg, std_msgs::Float64* relative_altitude);
 
   ros::NodeHandle nh_;
   sensor_msgs::NavSatFix home_gps_{};
   bool is_home_gps_set_ = false;
   ros::Rate rate_ = ros::Rate(20.0);
-
   boost::thread* thread_watch_alt_ = nullptr;  // for watching drone's altitude
-  void watchAltitudeThread();
-  void getRelativeAltitudeCB(const std_msgs::Float64ConstPtr& msg, std_msgs::Float64* relative_altitude);
 };
 
 #endif
