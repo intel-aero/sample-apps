e088ea71cbea7a1d0da3de17e78b93b69bd9b6f6
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2017-12-12 14:45:32 +0530

    Improvements to aero_takeoff_land, aero_telemetry_simple.
    1. Simplified aero_takeoff_land by abstracting AeroDrone actions in its class.
    2. Simplified aero_telemetry_simple by abstracting AeroDrone telemetry in its class.

--- ros/src/aero_takeoff_land/src/aero_takeoff_land_node.cpp
+++ ros/src/aero_takeoff_land/src/aero_takeoff_land_node.cpp
@@ -5,28 +5,57 @@
 */
 
 #include <cstdlib>
-#include "aero_drone.h"
+#include "aero_drone_action.h"
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "aero_takeoff_land");
   AeroDrone aero;
+  int ret = EXIT_SUCCESS;
 
+  // Arm
   if (!aero.arm())
-    exit(EXIT_FAILURE);
+  {
+    ROS_ERROR("Fatal: Arming failed!");
+    ret = EXIT_FAILURE;
+    goto end;
+  }
+  else
+  {
+    ROS_INFO("Arm sent");
+  }
 
+  // Takeoff
   if (!aero.takeoff())
-    exit(EXIT_FAILURE);
+  {
+    ROS_ERROR("Fatal: Takeoff failed!");
+    ret = EXIT_FAILURE;
+    goto end;
+  }
+  else
+  {
+    ROS_INFO("Takeoff sent");
+  }
 
   sleep(5);  // Let Aero reach takeoff altitude
 
+  // Land
   if (!aero.land())
-    exit(EXIT_FAILURE);
+  {
+    ROS_ERROR("Fatal: Land failed!");
+    ret = EXIT_FAILURE;
+    goto end;
+  }
+  else
+  {
+    ROS_INFO("Land sent");
+  }
 
   sleep(5);  // Let Aero land..
 
   ROS_INFO("Done");
+end:
   ros::shutdown();
 
-  return 0;
+  return ret;
 }
