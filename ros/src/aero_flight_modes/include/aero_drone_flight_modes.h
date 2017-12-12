9bcedf4af77b39a01f5b4adad0a9495d398c49c1
Shakthi Prashanth M <shakthi.prashanth.m@intel.com>
2017-12-12 18:40:20 +0530

    aero_flight_modes: Improvements.
    1. Added AeroDrone class to keep consistent with other examples.

---
+++ ros/src/aero_flight_modes/include/aero_drone_flight_modes.h
@@ -0,0 +1,37 @@
+#ifndef __AERO_DRONE_ACTION_H__
+#define __AERO_DRONE_ACTION_H__
+
+#include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
+#include <ros/ros.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <mavros_msgs/HomePosition.h>
+#include <mavros_msgs/State.h>
+#include <std_msgs/Float64.h>
+
+class AeroDrone
+{
+public:
+  AeroDrone();
+  ~AeroDrone();
+
+  // Actions
+  bool arm();
+  bool takeoff();
+  bool land();
+
+private:
+  void resetHome();
+  void getHomeGeoPoint();
+  void setHomeGeoPointCB(const mavros_msgs::HomePositionConstPtr& home);
+  void watchFlightModeThread();
+  void printFlightModeCB(const mavros_msgs::StateConstPtr& mode);
+
+  ros::NodeHandle nh_;
+  mavros_msgs::HomePosition home_{};
+  bool home_set_ = false;
+  ros::Rate rate_ = ros::Rate(20.0);
+  boost::thread* thread_watch_flight_mode_ = nullptr;  // for watching Aero's flight modes
+};
+
+#endif
--- ros/src/aero_flight_modes/src/aero_flight_modes_node.cpp
+++ ros/src/aero_flight_modes/src/aero_flight_modes_node.cpp
@@ -5,94 +5,57 @@
 */
 
 #include <cstdlib>
-#include <ros/ros.h>
-#include <sensor_msgs/NavSatFix.h>
-#include <mavros_msgs/CommandBool.h>
-#include <mavros_msgs/CommandTOL.h>
-#include <mavros_msgs/SetMode.h>
-#include <mavros_msgs/State.h>
-#include <boost/bind.hpp>
-#include <string.h>
-#include <pthread.h>
-#include <boost/thread/thread.hpp>
-
-// printing flight mode
-void getFlightModeSubCb(const mavros_msgs::StateConstPtr& msg, mavros_msgs::State* flight_mode)
-{
-  *flight_mode = *msg;
-  ROS_INFO("mode: %s", flight_mode->mode.c_str());
-}
-
-// subscribing to mavros/state to get flight mode updates
-void watchFlightMode()
-{
-  mavros_msgs::State flight_mode;
-  ros::NodeHandlePtr node = boost::make_shared<ros::NodeHandle>();
-  ros::Subscriber flight_mode_sub =
-      node->subscribe<mavros_msgs::State>("mavros/state", 3, boost::bind(getFlightModeSubCb, _1, &flight_mode));
-  while (ros::ok())
-  {
-    ros::spinOnce();
-  }
-}
+#include "aero_drone_flight_modes.h"
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "aero_flight_modes");
-  ros::NodeHandle nh;
-  boost::thread thread_mode(watchFlightMode);
+  AeroDrone aero;
+  int ret = EXIT_SUCCESS;
 
-  const float ALTITUDE = 500;
-  const float LATITUDE = 47.3977415;
-  const float LONGITUDE = 8.5455937;
-  const float MIN_PITCH = 0;
-  const float YAW = 0;
-  // arming
-  ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
-  mavros_msgs::CommandBool srv_arm;
-  srv_arm.request.value = true;
-  if (arming_client.call(srv_arm) && srv_arm.response.success)
-    ROS_INFO("ARM sent %d", srv_arm.response.success);
+  // Arm
+  if (!aero.arm())
+  {
+    ROS_ERROR("Fatal: Arming failed!");
+    ret = EXIT_FAILURE;
+    goto end;
+  }
   else
   {
-    ROS_ERROR("Failed arming/disarming");
-    ros::shutdown();
-    return -1;
+    ROS_INFO("Arm sent");
   }
 
-  // takeoff
-  ros::ServiceClient takeoff_client = nh.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
-  mavros_msgs::CommandTOL srv_takeoff;
-  srv_takeoff.request.altitude = ALTITUDE;
-  srv_takeoff.request.latitude = LATITUDE;
-  srv_takeoff.request.longitude = LONGITUDE;
-  srv_takeoff.request.min_pitch = MIN_PITCH;
-  srv_takeoff.request.yaw = YAW;
-  if (takeoff_client.call(srv_takeoff) && srv_takeoff.response.success)
-    ROS_INFO("TAKEOFF sent %d", srv_takeoff.response.success);
+  // Takeoff
+  if (!aero.takeoff())
+  {
+    ROS_ERROR("Fatal: Takeoff failed!");
+    ret = EXIT_FAILURE;
+    goto end;
+  }
   else
   {
-    ROS_ERROR("Failed Takeoff");
-    ros::shutdown();
-    return -1;
+    ROS_INFO("Takeoff sent");
   }
 
-  sleep(10);
+  sleep(5);  // Let Aero reach takeoff altitude
 
-  // land
-  ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
-  mavros_msgs::CommandTOL srv_land{};
-  if (land_client.call(srv_land) && srv_land.response.success)
-    ROS_INFO("land sent %d", srv_land.response.success);
+  // Land
+  if (!aero.land())
+  {
+    ROS_ERROR("Fatal: Land failed!");
+    ret = EXIT_FAILURE;
+    goto end;
+  }
   else
   {
-    ROS_ERROR("Failed Land");
-    ros::shutdown();
-    return -1;
+    ROS_INFO("Land sent");
   }
 
-  sleep(5);
+  sleep(5);  // Let Aero land..
+
   ROS_INFO("Done");
+end:
   ros::shutdown();
-  return 0;
+
+  return ret;
 }
